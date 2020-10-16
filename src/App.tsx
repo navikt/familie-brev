c572044123df5273c8dc1d94e101cdbfe8758996
mundal <halvorbmundal@gmail.com>
2020-10-16 12:33:21 +0200

    poc/variabler

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import { client } from './utils/sanity';
-const BlockContent = require('@sanity/block-content-to-react')
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { client } from "./utils/sanity";
+const BlockContent = require("@sanity/block-content-to-react");
 
 function App() {
   const [info, setInfo] = useState<any>();
+  const [tittel, setTittel] = useState<string>("");
 
-  const query = '*[_type == "submal"][0]';
+  const duFaar = {
+    fom: "'PLACEHOLDER FOMDATO'",
+    tom: "'PLACEHOLDER TOMDATO'",
+    belop: "placeholder beløp",
+    antallBarn: 3,
+    barnasFodselsdatoer: "placeholder barnas fødselsdatoer",
+    begrunnelser: ["INNVILGET_BOSATT_I_RIKTET"],
+  };
 
-    useEffect(() => {
-      client
-          .fetch(query)
-          .then((res: any) => {
-              setInfo(res.submal_block);
-          });
+  const variableSerializer = (props: any) => {
+    // @ts-ignore
+    return duFaar[props.children[0]];
+  };
+
+  const query = '*[_type == "test"][0]';
+
+  useEffect(() => {
+    client.fetch(query).then((res: any) => {
+      setInfo(res.submal_block);
+      setTittel(res.tittel);
+    });
   }, []);
-
   return (
     <div className="App">
-          {info && <BlockContent blocks={info}/>}
+      <h1>{tittel}</h1>
+      {info && (
+        <BlockContent
+          blocks={info}
+          serializers={{
+            marks: { variable: variableSerializer },
+          }}
+        />
+      )}
     </div>
   );
 }
