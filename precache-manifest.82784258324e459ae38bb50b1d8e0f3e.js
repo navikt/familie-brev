6daf79fd4f59a7f2d2a522285429fe320bf42aca
Asbjørn Steinskog <asbjorn@steinskog.me>
2020-11-03 11:08:14 +0100

    Updates

---
+++ precache-manifest.82784258324e459ae38bb50b1d8e0f3e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fc4833d5d984f3cc8df3ebc519339f5",
+    "url": "/familie-brev/index.html"
+  },
+  {
+    "revision": "0e6e796cde0ccc158006",
+    "url": "/familie-brev/static/css/2.03ef88f4.chunk.css"
+  },
+  {
+    "revision": "8bf95eef76fcefe8607f",
+    "url": "/familie-brev/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0e6e796cde0ccc158006",
+    "url": "/familie-brev/static/js/2.66fb063e.chunk.js"
+  },
+  {
+    "revision": "e5bdd165dc2357d53ac4a6296dae8024",
+    "url": "/familie-brev/static/js/2.66fb063e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bf95eef76fcefe8607f",
+    "url": "/familie-brev/static/js/main.a2d6bf72.chunk.js"
+  },
+  {
+    "revision": "a3b7a2de074ef5221a67",
+    "url": "/familie-brev/static/js/runtime-main.fb7a4edf.js"
+  },
+  {
+    "revision": "a757e3fb2b52f23fbdf4f0a85416ed44",
+    "url": "/familie-brev/static/media/nav-logo-red.a757e3fb.svg"
+  }
+]);
