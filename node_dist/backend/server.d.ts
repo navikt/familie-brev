842b569fdea1ff4d5a0c62af1a253e3079aed2b8
Asbjørn Steinskog <asbjorn@steinskog.me>
2020-11-24 12:42:24 +0100

    Kjør

---
+++ node_dist/backend/server.d.ts
@@ -0,0 +1,9 @@
+declare var path: any;
+declare const express: any;
+declare const jsdom: any;
+declare const request: any;
+declare const mustacheExpress: any;
+declare const app: any;
+declare const port = 8080;
+declare const router: any;
+declare const JSDOM: any;
