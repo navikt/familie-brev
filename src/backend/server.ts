3493cb6afba995131ba5120e9f0e3a466a107afd
Asbjørn Steinskog <asbjorn@steinskog.me>
2020-12-01 12:51:21 +0100

    Deploy

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -23,6 +23,10 @@ app.set("views", `${__dirname}/../../build`);
 app.set("view engine", "mustache");
 app.engine("html", mustacheExpress());
 
+app.get('/status', (req: any, res: any) => {
+    res.status(200).end();
+});
+
 app.use("/", router);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
