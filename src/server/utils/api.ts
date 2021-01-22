c06859a6782993cf0206478eb830a12769cd4b44
Henning Håkonsen <henning.hakonsen@nav.no>
2021-01-22 10:29:11 +0100

    Fjerner frontend.

--- src/server/utils/api.ts
+++ src/server/utils/api.ts
@@ -4,6 +4,7 @@ import { hentMiljøvariabler } from '../environment';
 export const genererPdf = async (html: string): Promise<ArrayBuffer> => {
   const url = `${hentMiljøvariabler().FAMILIE_DOKUMENT_API_URL}/api/html-til-pdf`;
 
+  console.log(`Genererer pdf mot ${url}`);
   return axios
     .post(url, html, {
       responseType: 'arraybuffer',
