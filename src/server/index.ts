84cf23a91fb5506b88e38eed38d8e4c9be810ca3
mundal <halvorbmundal@gmail.com>
2021-04-14 15:58:25 +0200

    fix

--- src/server/index.ts
+++ src/server/index.ts
@@ -10,12 +10,12 @@ export const { NODE_ENV } = process.env;
 const buildDir = path.join(process.cwd() + '/build');
 const app = express();
 
-app.use(express.json({ limit: '1mb' }));
-app.use(express.urlencoded({ extended: true }));
-
 if (NODE_ENV === 'production') {
   app.use(express.static(buildDir));
 }
+
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', routes);
 
