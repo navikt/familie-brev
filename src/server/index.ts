ed12e9f0867dcc574f0ee55e8f31bf6c4f189bb1
mundal <halvorbmundal@gmail.com>
2021-04-14 15:57:19 +0200

    fix

--- src/server/index.ts
+++ src/server/index.ts
@@ -10,13 +10,16 @@ export const { NODE_ENV } = process.env;
 const buildDir = path.join(process.cwd() + '/build');
 const app = express();
 
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended: true }));
+
 if (NODE_ENV === 'production') {
   app.use(express.static(buildDir));
 }
 
 app.use('/api', routes);
 
-const port = 8001;
+const port = 8000;
 app.listen(port, () => {
   logInfo(`Server now listening on port: ${port}`);
 });
