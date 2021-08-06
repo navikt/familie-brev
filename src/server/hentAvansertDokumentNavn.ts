00e9f3bf8be74afd1242491151e170c4ac0ed306
Asbjørn Steinskog <asbjorn@steinskog.me>
2021-08-06 10:41:18 +0200

    Legg til endepunkt for å hente navn på avanserte dokumenter
    Brukes i familie-ef-sak-frontend for å kunne velge hvilken brevmal
    man vil bruke.

---
+++ src/server/hentAvansertDokumentNavn.ts
@@ -0,0 +1,13 @@
+import { Maalform } from '../typer/sanitygrensesnitt';
+import { client, Datasett } from './sanity/sanityClient';
+import { Feil } from './utils/Feil';
+
+export const hentAvansertDokumentNavn = async (datasett: Datasett): Promise<string> => {
+  const query = `*[_type == "dokumentmal"]{visningsnavn, apiNavn}`;
+
+  return client(datasett)
+    .fetch(query)
+    .catch(error => {
+      throw new Feil(error.message, error.statusCode);
+    });
+};
