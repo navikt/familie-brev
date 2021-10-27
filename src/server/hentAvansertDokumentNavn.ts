124fdc7238034633ab4d0c724ce31c00cd6658c9
Asbjørn Steinskog <asbjorn@steinskog.me>
2021-10-27 15:16:48 +0200

    Bare hent publiserte dokumenter

--- src/server/hentAvansertDokumentNavn.ts
+++ src/server/hentAvansertDokumentNavn.ts
@@ -2,7 +2,7 @@ import { client, Datasett } from './sanity/sanityClient';
 import { Feil } from './utils/Feil';
 
 export const hentAvansertDokumentNavn = async (datasett: Datasett): Promise<string> => {
-  const query = `*[_type == "dokumentmal"]{visningsnavn, apiNavn}`;
+  const query = `*[_type == "dokumentmal" && publisert == true]{visningsnavn, apiNavn}`;
 
   return client(datasett)
     .fetch(query)
