a45959928a11140b1b5bfac153c98a612aedffff
eirarset <eirik.arseth@nav.no>
2021-12-03 15:29:57 +0100

    Skal kunne hente upubliserte brevmaler
    Co-authored-by: Mattis Janitz <mattis.janitz@nav.no>

--- src/server/hentAvansertDokumentNavn.ts
+++ src/server/hentAvansertDokumentNavn.ts
@@ -1,8 +1,16 @@
 import { client, Datasett } from './sanity/sanityClient';
 import { Feil } from './utils/Feil';
 
-export const hentAvansertDokumentNavn = async (datasett: Datasett): Promise<string> => {
-  const query = `*[_type == "dokumentmal" && publisert == true]{visningsnavn, apiNavn}`;
+export const hentAvansertDokumentNavn = async (
+  datasett: Datasett,
+  hentUpubliserte?: boolean,
+): Promise<string> => {
+  let query;
+  if (hentUpubliserte) {
+    query = `*[_type == "dokumentmal"]{visningsnavn, apiNavn}`;
+  } else {
+    query = `*[_type == "dokumentmal" && publisert == true]{visningsnavn, apiNavn}`;
+  }
 
   return client(datasett)
     .fetch(query)
