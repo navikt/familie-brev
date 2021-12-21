6d87ee431e4f3d7089eb762f3927f1461e9157c4
halvormundal <halvorbmundal@gmail.com>
2021-12-21 12:09:33 +0100

    Bruk "Namn" som tittel på navnefeltet når brevet er nynorsk

--- src/server/components/Header.tsx
+++ src/server/components/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavIkon } from './ikoner/navIkon';
 import { Flettefelt } from '../../typer/dokumentApi';
 import { validerFlettefelt } from '../utils/valideringer/validerFlettefelt';
+import { Maalform } from '../../typer/sanitygrensesnitt';
 
 interface HeaderProps {
   tittel: string;
@@ -10,10 +11,11 @@ interface HeaderProps {
   brevOpprettetDato: Flettefelt;
   visLogo?: boolean;
   apiNavn: string;
+  maalform: Maalform;
 }
 
 function Header(props: HeaderProps) {
-  const { tittel, navn, fodselsnummer, visLogo, brevOpprettetDato, apiNavn } = props;
+  const { tittel, navn, fodselsnummer, visLogo, brevOpprettetDato, apiNavn, maalform } = props;
 
   validerFlettefelt(navn, 'navn', apiNavn, false);
   validerFlettefelt(fodselsnummer, 'fodselsnummer', apiNavn, false);
@@ -29,7 +31,9 @@ function Header(props: HeaderProps) {
         <h2 className="tittel">{tittel.toUpperCase()}</h2>
         <div className="kolonner">
           <div className="personinfo">
-            <div>Navn: {navn}</div>
+            <div>
+              {navnTittel(maalform)}: {navn}
+            </div>
             <div>Fødselsnummer: {fodselsnummer}</div>
           </div>
         </div>
@@ -37,5 +41,14 @@ function Header(props: HeaderProps) {
     </div>
   );
 }
+
+const navnTittel = (maalform: Maalform): string => {
+  switch (maalform) {
+    case Maalform.NB:
+      return 'Navn';
+    case Maalform.NN:
+      return 'Namn';
+  }
+};
 
 export default Header;
