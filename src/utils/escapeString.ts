6bd81a5b03ef7befae85f317f94dca7e8db22c3f
halvormundal <halvorbmundal@gmail.com>
2022-11-10 13:49:25 +0100

    Ikke escape æøå

--- src/utils/escapeString.ts
+++ src/utils/escapeString.ts
@@ -1,2 +1,2 @@
 export const escape = (tekst: string) =>
-  tekst.replace(/[^0-9A-Za-z ]/g, c => '&#' + c.charCodeAt(0) + ';');
+  tekst.replace(/[^0-9A-ZÆØÅa-zæøå ]/g, c => '&#' + c.charCodeAt(0) + ';');
