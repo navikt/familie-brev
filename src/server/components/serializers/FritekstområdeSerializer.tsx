7b46596354c868fb0b901373da6d7a4e538d09a9
Sara Hjelle <46678893+sarahjelle@users.noreply.github.com>
2023-03-13 09:38:45 +0100

    Fritekstområde sanitybrev (#430)
    * Skal hente fritekstområder for avansertDokument
    
    * Skal kunne vise fritekstområder i avansert dokument
    
    Co-authored-by: Sara Hjelle <sara.hjelle@nav.no>
    
    * Skal ta med deloverskrifter og linjeskift i fritekstområder
    
    * Skal kun mappe gjennom avsnitt dersom de eksisterer
    
    * Felt skal ha mer meningsbærende navn
    
    ---------
    
    Co-authored-by: eirarset <eirik.arseth@nav.no>

---
+++ src/server/components/serializers/FritekstområdeSerializer.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import type { IFritekstområder } from '../../../typer/dokumentApi';
+
+interface IFritekstområdeSerializerProps {
+  sanityProps: any;
+  fritekstområder?: IFritekstområder;
+}
+
+export const FritekstområdeSerializer = (props: IFritekstområdeSerializerProps) => {
+  const { sanityProps, fritekstområder } = props;
+
+  if (!fritekstområder) return null;
+
+  const uuid = sanityProps.value._key;
+  const avsnitt = fritekstområder[uuid];
+
+  return (
+    <div>
+      {avsnitt &&
+        avsnitt.map((avsnitt, index) => (
+          <p key={index}>
+            {avsnitt.deloverskrift && <strong>{avsnitt.deloverskrift} </strong>}
+            {avsnitt.deloverskrift && <br />}
+            {avsnitt.innhold && <span style={{ whiteSpace: 'pre-wrap' }}>{avsnitt.innhold}</span>}
+          </p>
+        ))}
+    </div>
+  );
+};
