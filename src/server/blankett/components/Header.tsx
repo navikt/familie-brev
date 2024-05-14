fdf06e00042d31b0e8e49168d928e7577b2c8116
bragejahren <brage.ekroll.jahren@nav.no>
2024-05-14 08:15:32 +0200

    Legger på <div className='ikon-og-dato-wrapper'> alle steder vi bruker 'ikon-og-dato' for at styling skal bli korrekt

--- src/server/blankett/components/Header.tsx
+++ src/server/blankett/components/Header.tsx
@@ -14,9 +14,11 @@ function Header(props: HeaderProps) {
 
   return (
     <div className={'header'}>
-      <div className="ikon-og-dato">
-        {visLogo && <NavIkon />}
-        <p>{dato}</p>
+      <div className="ikon-og-dato-wrapper">
+        <div className="ikon-og-dato">
+          {visLogo && <NavIkon />}
+          <p>{dato}</p>
+        </div>
       </div>
       <div className={'tittel-og-personinfo'}>
         <h2 className="tittel">{tittel.toUpperCase()}</h2>
