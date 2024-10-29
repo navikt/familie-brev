ad388d186ae389de9173b2636b1f74a100d56859
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2024-10-29 08:59:49 +0100

    Undo, for å sjekke om det ikke ble feil

--- src/server/components/Brevhode.tsx
+++ src/server/components/Brevhode.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NavIkon } from './ikoner/navIkon';
-import { lagStorForbokstav } from '../utils/strenghåndtering';
 
 interface BrevhodeProps {
   tittel: string;
@@ -21,7 +20,7 @@ export const Brevhode = (props: BrevhodeProps) => {
         </div>
       </div>
       <div className={'tittel-og-personinfo'}>
-        <h2 className="tittel">{lagStorForbokstav(tittel)}</h2>
+        <h2 className="tittel">{tittel.toUpperCase()}</h2>
         <div className="kolonner">
           <div className="personinfo">
             <div>Navn: {navn}</div>
