21947b624075f1fdfbeff68aae6bbd916b8561b6
charliemidtlyng <charlie.midtlyng@bekk.no>
2024-11-11 12:39:44 +0100

    Skal endre fra NAV til Nav i tekster

--- src/typer/dokumentApiBlankett.ts
+++ src/typer/dokumentApiBlankett.ts
@@ -785,7 +785,7 @@ export const delvilkårTypeTilTekst: Record<IRegelId, string> = {
   DOKUMENTASJON_AV_UTDANNING: 'Er det dokumentert at bruker er under utdanning?',
   DOKUMENTASJON_AV_UTGIFTER_UTDANNING:
     'Er det dokumentert at brukeren har utgifter til utdanningen?',
-  NAVKONTOR_VURDERING: 'Har NAV-kontoret vurdert utdanningen?',
+  NAVKONTOR_VURDERING: 'Har Nav-kontoret vurdert utdanningen?',
   SAKSBEHANDLER_VURDERING: 'Er utdanningen nødvendig og hensiktsmessig?',
 };
 
