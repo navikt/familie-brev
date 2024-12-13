0d7751adba779268dc37f221cf4cdc6c54d2d4ed
Charlie Midtlyng <charlie.midtlyng@bekk.no>
2024-12-13 14:10:05 +0100

    Skal kunne omgjøre delmalBlock til html for å kunne tilby htmlFelt so… (#730)
    * Skal kunne omgjøre delmalBlock til html for å kunne tilby htmlFelt som overstyrer innholdet
    
    * Skal kunne overstyre innhold basert på overstyrtDelmalBlokk
    
    * Skal ha delmal som input for konvertering av delmal til html
    
    * Vil returnere en ressurs når vi konverterer delmalblokk til html
    
    * Skal ha htmlInnhold og skalOverstyre som variabler i overstyrtDelmalblokk
    
    * Skal gjøre om html fra quill som sender inn ol istedenfor ul med data-attributtet bullet-list

--- src/server/components/AvansertDokument.tsx
+++ src/server/components/AvansertDokument.tsx
@@ -48,6 +48,17 @@ function AvansertDokument(avansertDokumentProps: AvansertDokumentProps) {
     return null;
   }
 
+  if (avanserteDokumentVariabler?.overstyrtDelmalblokk?.skalOverstyre) {
+    // TODO: Håndtere htmlTabell her
+    return (
+      <div
+        dangerouslySetInnerHTML={{
+          __html: avanserteDokumentVariabler.overstyrtDelmalblokk.htmlInnhold,
+        }}
+      />
+    );
+  }
+
   return (
     <PortableText
       value={avansertDokument}
