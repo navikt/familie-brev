bdde124829bcd2a4fc2a22c05e00c38357982250
throndi <56258085+throndi@users.noreply.github.com>
2025-02-07 14:53:14 +0100

    ...

--- src/server/hentAvansertDokumentHtml.tsx
+++ src/server/hentAvansertDokumentHtml.tsx
@@ -81,7 +81,7 @@ export const hentAvansertDokumentHtml = async (
                     <span style={{ marginRight: '20px' }}>{besluttersignatur}</span>
                   </>
                 )}
-                <span>{saksbehandlersignatur}</span>
+                <span>{saksbehandlersignatur} saksbehandler</span>
               </p>
             </div>
           </div>
