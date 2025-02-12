da6b7aa42362ffb1dc46951dd71efd8b95b670b3
throndi <56258085+throndi@users.noreply.github.com>
2025-02-12 11:19:55 +0100

    fjerner forrige test, rettelse

--- src/server/hentAvansertDokumentHtml.tsx
+++ src/server/hentAvansertDokumentHtml.tsx
@@ -76,7 +76,7 @@ export const hentAvansertDokumentHtml = async (
             <div>
               <div>{enhet || 'Nav Arbeid og ytelser'}</div>
               <p style={{ float: 'left' }}>
-                {skjulBeslutterSignatur && (
+                {!skjulBeslutterSignatur && (
                   <>
                     <span style={{ marginRight: '20px' }}>{besluttersignatur}</span>
                   </>
