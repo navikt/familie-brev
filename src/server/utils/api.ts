6a41af86d7e24ee740de4c224616464526c8e119
RobinLundSadun <robin.lund.sadun@gmail.com>
2026-04-13 11:10:10 +0200

    Bytter ut axios med fetch

--- src/server/utils/api.ts
+++ src/server/utils/api.ts
@@ -1,5 +1,3 @@
-import type { AxiosResponse } from 'axios';
-import axios from 'axios';
 import { hentMiljøvariabler } from '../environment';
 import { Feil } from './Feil';
 import { logInfo } from '@navikt/familie-logging';
@@ -8,16 +6,19 @@ export const genererPdf = async (html: string): Promise<ArrayBuffer> => {
   const url = `${hentMiljøvariabler().FAMILIE_DOKUMENT_API_URL}/api/html-til-pdf`;
 
   logInfo(`Generer pdf mot ${url}`);
-  return axios
-    .post(url, html, {
-      responseType: 'arraybuffer',
-      headers: {
-        'Content-Type': 'text/html',
-        Accept: 'application/pdf',
-      },
-    })
-    .then((res: AxiosResponse<ArrayBuffer>) => res.data)
-    .catch(error => {
-      throw new Feil(`Feil mot familie-dokument`, 500, error);
-    });
+
+  const res = await fetch(url, {
+    method: 'POST',
+    body: html,
+    headers: {
+      'Content-Type': 'text/html',
+      Accept: 'application/pdf',
+    },
+  });
+
+  if (!res.ok) {
+    throw new Feil(`Feil mot familie-dokument`, 500);
+  }
+
+  return res.arrayBuffer();
 };
--- src/server/utils/apiBlankett.ts
+++ src/server/utils/apiBlankett.ts
@@ -1,5 +1,3 @@
-import type { AxiosResponse } from 'axios';
-import axios from 'axios';
 import { hentMiljøvariabler } from '../environment';
 import type { Meta } from '@navikt/familie-logging';
 import { logInfo } from '@navikt/familie-logging';
@@ -8,18 +6,21 @@ export const genererPdfBlankett = async (html: string, meta: Meta): Promise<Arra
   const url = `${hentMiljøvariabler().FAMILIE_DOKUMENT_API_URL}/api/html-til-pdf`;
 
   logInfo(`Generer pdf mot ${url}`, meta);
-  return axios
-    .post(url, html, {
-      responseType: 'arraybuffer',
-      headers: {
-        'Content-Type': 'text/html',
-        Accept: 'application/pdf',
-        'Nav-Consumer-Id': 'familie-ef-blankett',
-      },
-    })
-    .then((res: AxiosResponse<ArrayBuffer>) => res.data)
-    .catch(error => {
-      logInfo('Feil mot familie-dokument', meta);
-      throw error;
-    });
+
+  const res = await fetch(url, {
+    method: 'POST',
+    body: html,
+    headers: {
+      'Content-Type': 'text/html',
+      Accept: 'application/pdf',
+      'Nav-Consumer-Id': 'familie-ef-blankett',
+    },
+  });
+
+  if (!res.ok) {
+    logInfo('Feil mot familie-dokument', meta);
+    throw new Error(`Feil mot familie-dokument: ${res.status}`);
+  }
+
+  return res.arrayBuffer();
 };
