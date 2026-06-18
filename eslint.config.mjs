74f06823e14b731d3cc005549160334552d26f89
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2026-06-18 10:37:12 +0200

    Legger tilbake eslint regler, men kun som warning og versjonssikre varianter som brukes.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,11 @@ import tseslint from 'typescript-eslint';
 import react from 'eslint-plugin-react';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
 
+const reactHooksOptionalWarnRules = {
+  ...(reactHooks.rules['static-components'] ? { 'react-hooks/static-components': 'warn' } : {}),
+  ...(reactHooks.rules.immutability ? { 'react-hooks/immutability': 'warn' } : {}),
+};
+
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
@@ -20,6 +25,7 @@ export default tseslint.config(
     plugins: { 'react-hooks': reactHooks },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      ...reactHooksOptionalWarnRules,
       'react/no-unescaped-entities': 'off',
       '@typescript-eslint/no-unused-expressions': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
